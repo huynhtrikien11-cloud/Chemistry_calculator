01e155dcec8a8a06c0973d1874784d54e5268596
huynhtrikien11-cloud <huynhtrikien11-cloud@users.noreply.github.com>
2026-07-20 13:20:40 +0700

    Publish Sukuna Gojo story

---
+++ static/js/stories.js
@@ -0,0 +1,91 @@
+const StaticStories = [
+    {
+        id: 'sukuna-gojo-war',
+        title: 'Dai chien giua Sukuna va Gojo',
+        author: 'huynhtrikien11-cloud',
+        description: 'Truyen chu online ve tran dai chien giua Sukuna va Gojo, viet theo phong cach hoi thoai va tung phan ngan.',
+        storyText: `-----Dai chien giua Sukuna va Gojo-----
+So truyen: 1
+
+Gojo: VO LUONG KHONG XU!
+
+Sukuna: PHUC MA NGU TRU TU!
+
+Sukuna: HAHAHA! NHAT CHEM THE GIOI! GOJO, TA TOI DAY!
+
+Gojo: Ua...gi vay?
+
+*Gojo da ra di*
+
+Sukuna: HAHAHAHAHA
+
+*Hakari chay toi*
+
+Hakari: THAY OI, THAY GOJO? THAY GOJOOOOOO!
+
+Hakari: SUKUNA, NGUOI SE BIET TAY!
+
+Hakari: MO RONG LANH DIA, TOA SAT BAT DO!!!
+
+Lan nay quay ra so: 2
+
+Lan nay quay ra so: 4
+
+Lan nay quay ra so: 5
+
+JACKPOT!
+
+--Se co tiep phan 2. Moi cac ban don doc--
+
+-----Dai chien giua Sukuna va Gojo-----
+So truyen: 2
+
+Sukuna: HAHAHA! Cung chi la 1 thang nhai biet nhay nhot lon xon
+
+*Suy nghi cua Sukuna: Thang nay khong binh thuong*
+
+Higuruma: Con ta nua ne!
+
+Higuruma: TRU PHUC TU TU!
+
+Tham phan: moi hanh vi bao luc o day deu bi CAM!
+
+Tham phan: So vach toi hien gio cua Sukuna: 0/3
+
+Higuruma phan: SILENCE
+
+Sukuna khai: SILENCE
+
+Ca 2 deu im lang! Sukuna bi tang 1 vach toi!
+
+Tham phan: So vach toi hien gio cua Sukuna: 1/3
+
+Higuruma phan: SILENCE
+
+Sukuna khai: DECLINE
+
+Higuruma bat thop duoc ly le cua Sukuna! Tang 1 vach toi!
+
+Tham phan: So vach toi hien gio cua Sukuna: 2/3
+
+Higuruma phan: CONFIRM
+
+Sukuna khai: CONFIRM
+
+Sukuna da nhan toi! Ban an tu hinh duoc thiet lap!
+
+Kiem dao phu duoc trao cho Higuruma!
+
+*Phien toa ket thuc!*
+
+Higuruma: Hakari! Hay hop suc lai de danh bai Sukuna va bao thu cho thay Gojo!
+
+Hakari: DUOC! VI THAY GOJO, VI MOI NGUOI!!!
+
+Ca hai: XONG LEN, CHET DI, SUKUNAAAA
+
+--Se co tiep phan 3. Moi cac ban don doc--`
+    }
+];
+
+window.StaticStories = StaticStories;
